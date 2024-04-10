e261e5d32bfe91e37fd93732c1d398d8ad368fd7
gkqgksdjf <72592919+gkqgksdjf@users.noreply.github.com>
2024-04-10 18:27:11 +0900

    <<24.04.10>> "TrashFlight"
    add Assets

--- My project/Assets/Scripts/EnemySpawner.cs
+++ My project/Assets/Scripts/EnemySpawner.cs
@@ -8,20 +8,73 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField]
     private GameObject[] enemies;
 
+    [SerializeField]
+    private GameObject boss;
+
     private float[] arrPosX = {-2.2f, -1.1f, 0f, 1.1f, 2.2f};
+
+    [SerializeField]
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        // arrPosX에서 값을 하나씩 꺼내서 posX에 집어 넣음
-        foreach(float posX in arrPosX){
-            int index = UnityEngine.Random.Range(0, enemies.Length);
-            Spawnenemy(posX, index);   
-        }   
+        StartEnemyRoutine();
     }
 
-    void Spawnenemy(float posX, int index){
+    void StartEnemyRoutine(){
+        StartCoroutine("EnemyRoutine");
+    }
+
+    public void StopEnemyRoutine(){
+        StopCoroutine("EnemyRoutine");
+    }
+
+    IEnumerator EnemyRoutine() {
+        yield return new WaitForSeconds(3f);
+        
+        float moveSpeed = 5f;
+        int spawnCount = 0;
+        int enemyIndex = 0;
+
+        while(true){
+            // arrPosX에서 값을 하나씩 꺼내서 posX에 집어 넣음
+            foreach(float posX in arrPosX){
+                //int index = UnityEngine.Random.Range(0, enemies.Length);
+                Spawnenemy(posX, /*index*/enemyIndex, moveSpeed);   
+            }
+
+            spawnCount++;
+            if (spawnCount % 10 == 0){  // 10, 20, 30, ...
+                enemyIndex += 1;
+                moveSpeed += 2;
+            }
+
+            if(enemyIndex >= enemies.Length){
+                SpawnBoss();
+                enemyIndex = 0;
+                moveSpeed = 5f;
+            }
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    void Spawnenemy(float posX, int index, float moveSpeed){
         Vector3 spawnPos = new Vector3(posX, transform.position.y, transform.position.z);
-        Instantiate(enemies[index], spawnPos, quaternion.identity);
+
+        if(UnityEngine.Random.Range(0, 5) == 0){
+            index += 1;
+        }
+
+        if(index >= enemies.Length){
+            index = enemies.Length - 1;
+        }
+
+        GameObject enemy = Instantiate(enemies[index], spawnPos, quaternion.identity);
+    }
+
+    void SpawnBoss(){
+        Instantiate(boss, transform.position, Quaternion.identity);
     }
 }
--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -9,7 +9,8 @@ public class Player : MonoBehaviour
     private float moveSpeed;
 
     [SerializeField]
-    private GameObject weapon;
+    private GameObject[] weapons;
+    private int weaponIndex = 0;
 
     [SerializeField]
     private Transform shootTransform;
@@ -42,14 +43,33 @@ public class Player : MonoBehaviour
         float toX = Mathf.Clamp(mousePos.x, -2.35f, 2.35f);
         transform.position = new Vector3(toX, transform.position.y, transform.position.z);
 
-        Shoot();
+        if(GameManager.instance.isGameOver == false){
+            Shoot();
+        }
     }
 
     void Shoot(){
         if(Time.time - lastShotTime > shootInterval){
-            // quaternion.identity = 회전 x
-            Instantiate(weapon, shootTransform.position, quaternion.identity);
+            // 발사하는 무기 생성, quaternion.identity = 회전 x
+            Instantiate(weapons[weaponIndex], shootTransform.position, quaternion.identity);
             lastShotTime = Time.time;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag == "Enemy" || other.gameObject.tag == "Boss"){
+            GameManager.instance.SetGameOver();
+            Destroy(gameObject);
+        } else if(other.gameObject.tag == "Coin"){
+            GameManager.instance.IncreaseCoin();
+            Destroy(other.gameObject);
+        }
+    }
+
+    public void Upgrade(){
+        weaponIndex += 1;
+        if(weaponIndex >= weapons.Length){
+            weaponIndex = weapons.Length - 1;
         }
     }
 }
